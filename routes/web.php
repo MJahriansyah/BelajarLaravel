eba27d44952e9bcc505afffa0f9b65b58c5cce3c
Muhammad Jahriansyah <mjahriansyah@gmail.com>
2020-06-30 15:34:41 +0800

    new

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'HomeController@index');
-Route::get('/form','AuthController@register' );
+Route::get('/register','AuthController@register' );
 Route::post('/store','AuthController@store' );
 Route::get('/welcome','AuthController@welcome' );
